b9d08e41ba2bbf255c40cc9ccec7c8bcc02a3dd2
Uian Sol <usoldeveloper@gmail.com>
2023-12-26 12:05:00 -0300

    add create user usecase and move folders to core

--- internal/core/repositories/user_repository.go
+++ internal/core/repositories/user_repository.go
@@ -1,6 +1,6 @@
 package repositories
 
-import "github.com/uiansol/go-clean-architecture/internal/entities"
+import "github.com/uiansol/go-clean-architecture/internal/core/entities"
 
 type IUserRepository interface {
 	Save(user entities.User) (string, error)
---
+++ internal/core/usecases/create_user_usecase.go
@@ -0,0 +1,43 @@
+package usecases
+
+import (
+	"github.com/uiansol/go-clean-architecture/internal/core/entities"
+	"github.com/uiansol/go-clean-architecture/internal/core/repositories"
+)
+
+type CreateUserUseCase struct {
+	userRepository repositories.IUserRepository
+}
+
+type CreateUserInput struct {
+	Name  string
+	Email string
+}
+
+type CreateUserOutput struct {
+	UserID string
+}
+
+func NewCreateUserUseCase(userRepository repositories.IUserRepository) CreateUserUseCase {
+	return CreateUserUseCase{
+		userRepository: userRepository,
+	}
+}
+
+func (u *CreateUserUseCase) CreateUser(createUserInput CreateUserInput) (CreateUserOutput, error) {
+	user, err := entities.NewUser(createUserInput.Name, createUserInput.Email)
+	if err != nil {
+		return CreateUserOutput{}, err
+	}
+
+	id, err := u.userRepository.Save(user)
+	if err != nil {
+		return CreateUserOutput{}, err
+	}
+
+	output := CreateUserOutput{
+		UserID: id,
+	}
+
+	return output, nil
+}
